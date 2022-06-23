7bf8c449f7acc0c0941c17782db9181447ba4c57
polo_helcly <polo_helcly@qq.com>
2022-06-23 20:47:58 +0800

    初始化项目

--- src/main/java/com/qfsoft/app/atev/core/other/soccer/util/SoccerConstant.java
+++
@@ -1,7 +0,0 @@
-package com.qfsoft.app.atev.core.other.soccer.util;
-
-public class SoccerConstant {
-	
-	public final static String SEASON = "2021-2022";
-	
-}
--- src/main/java/com/qfsoft/app/atev/core/other/stock/util/StockConstant.java
+++
@@ -1,7 +0,0 @@
-package com.qfsoft.app.atev.core.other.stock.util;
-
-public class StockConstant {
-	
-	public final static String SEASON = "2022";
-	
-}
